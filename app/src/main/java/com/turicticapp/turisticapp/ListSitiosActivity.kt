c65b46c7cf754606daca94517e0c9b9740bdc29d
Juan C Ruiz <juanalgostudio@gmail.com>
2021-11-18 23:19:23 -0500

    JCR MockSitios to test RecyclerView in Lista POI

--- app/src/main/java/com/turicticapp/turisticapp/ListSitiosActivity.kt
+++ app/src/main/java/com/turicticapp/turisticapp/ListSitiosActivity.kt
@@ -27,5 +27,49 @@ class ListSitiosActivity : AppCompatActivity() {
         }
     }
 
+    private fun createMockSitios(): ArrayList<Sitio> {
+        /* Crear sitio uno a uno
+        Desde un formulario para agregar...
+        var lista: ArrayList<Sitio> = arrayListOf()
+         var sitio = Sitio(
+             name = "Pitalito",
+             description = "Capital del Surcolombiano",
+             score = "5.0"
+         )
+         lista.add(sitio)
+         return lista*/
 
+        return arrayListOf(
+            Sitio(
+                name = "Playa Spratt Bight",
+                description = "La playa más turística y más reconocida de San Andrés.",
+                score = "4.5 / 5.0"
+            ),
+            Sitio(
+                name = "Bahía Cocoplum",
+                description = "La playa más hermosa de San Andrés.",
+                score = "4.5 / 5.0"
+            ),
+            Sitio(
+                name = "Isla Johnny Cay",
+                description = "El Parque Regional protegido gracias a su biodiversidad y belleza.",
+                score = "4.5 / 5.0"
+            ),
+            Sitio(
+                name = "Cayo Acuario",
+                description = "Encuentro con la naturaleza y contacto con animales fantásticos.",
+                score = "4.5 / 5.0"
+            ),
+            Sitio(
+                name = "Mar de los siete colores",
+                description = "Esquina del Caribe con gama de tonos del mar, desde un azul profundo hasta un verde aguamarina translúcido.",
+                score = "4.5 / 5.0"
+            ),
+            Sitio(
+                name = "Cueva de Morgan",
+                description = "Museo Tributo a la leyenda de Morgan el Pirata.",
+                score = "3.0 / 5.0"
+            )
+        )
+    }
 }
