786bc99b8d17dbab119dffd5aa236d29c9674b38
Juan C Ruiz <juanalgostudio@gmail.com>
2021-11-18 20:40:57 -0500

    JCR First Commit Lista POI

---
+++ app/src/main/java/com/turicticapp/turisticapp/ListSitiosActivity.kt
@@ -0,0 +1,11 @@
+package com.turicticapp.turisticapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListSitiosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_sitios)
+    }
+}
