70c38bc45cc8cb8ab3ba138a5be4d39ffd8e2916
Juan C Ruiz <juanalgostudio@gmail.com>
2021-11-18 23:12:09 -0500

    JCR Tercer Commit Lista POI

--- app/src/main/java/com/turicticapp/turisticapp/ListSitiosActivity.kt
+++ app/src/main/java/com/turicticapp/turisticapp/ListSitiosActivity.kt
@@ -2,10 +2,30 @@ package com.turicticapp.turisticapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class ListSitiosActivity : AppCompatActivity() {
+
+    private lateinit var listSitios: ArrayList<Sitio>
+    private lateinit var sitiosAdapter: SitiosAdapter
+    private lateinit var sitiosRecyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_sitios)
+
+        sitiosRecyclerView = findViewById(R.id.sitios_recycler_view)
+
+        listSitios = createMockSitios()
+        sitiosAdapter = SitiosAdapter(listSitios)
+
+        sitiosRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = sitiosAdapter
+            setHasFixedSize(false)
+        }
     }
+
+
 }
---
+++ app/src/main/java/com/turicticapp/turisticapp/Sitio.kt
@@ -0,0 +1,7 @@
+package com.turicticapp.turisticapp
+
+data class Sitio(
+    val name: String,
+    val description: String,
+    val score: String
+)
